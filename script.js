998b6d13d091ea18401d249eba22361fc74e96e0
Hudson Baroni <hudsonbarni@gmail.com>
2022-05-14 14:55:24 -0300

    Scripts basicos - reset e display success|error

--- script.js
+++ script.js
@@ -4,15 +4,26 @@ function validaCarac(cpf) {
 
 }
 
+function reset() {
+    let CPFerror = document.getElementById('error');
+    let CPFsuccess = document.getElementById('success');
+    CPFsuccess.classList.remove('success');
+    CPFerror.classList.remove('error');
+}
+
 function validacao() {
     let cpf = document.getElementById('cpfDigitado').value;
 
     if (cpf != 0) {
+        let CPFerror = document.getElementById('error');
         let CPFsuccess = document.getElementById('success');
-        CPFsuccess.classList.toggle('success');
+        CPFsuccess.classList.add('success');
+        CPFerror.classList.remove('error');
     } else {
         let CPFerror = document.getElementById('error');
-        CPFerror.classList.toggle('error');
+        let CPFsuccess = document.getElementById('success');
+        CPFerror.classList.add('error');
+        CPFsuccess.classList.remove('success');
     }
 
 }

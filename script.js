37eed46357918ed22f785fc928cf75bb75cd06c2
Hudson Baroni <hudsonbarni@gmail.com>
2022-05-15 17:58:24 -0300

    Script de validação de digitos

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-console.log('JavaScrpt Carregado');
 
-function validaCarac(cpf) {
 
+function validaCpf(cpf) {
+    if (cpf.length == 11) {
+        let numeros = cpf.substring(0, 9);
+        let digitos = cpf.substring(9);
+        let soma = 0;
+        for (var i = 10; i > 1; i--) {
+            soma += numeros.charAt(10 - i) * i;
+        }
+        if (11 - (soma % 11) == digitos.charAt(0)) {
+            let numeros2 = cpf.substring(0, 10);
+            let soma2 = 0;
+            for (var i = 11; i > 1; i--) {
+                soma2 += numeros2.charAt(11 - i) * i;
+            }
+            if (11 - (soma2 % 11) == digitos.charAt(0)){
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
 }
 
 function reset() {
@@ -14,7 +37,7 @@ function reset() {
 function validacao() {
     let cpf = document.getElementById('cpfDigitado').value;
 
-    if (cpf != 0) {
+    if (validaCpf(cpf)) {
         let CPFerror = document.getElementById('error');
         let CPFsuccess = document.getElementById('success');
         CPFsuccess.classList.add('success');
